80b47f7a2685984012a2d39564bae009717af24c
BitJockey <bitjockey@jackpoint.network>
2020-03-21 23:40:56 -0500

    Basic AST

---
+++ discord_markdown/ast.py
@@ -0,0 +1,37 @@
+class Text:
+    def __init__(self, value):
+        self.value = value
+
+    def eval(self):
+        return self.value
+
+
+class FormattedText(Text):
+    HTML_TAG = ""
+
+    @property
+    def open(self):
+        return f"<{self.HTML_TAG}>"
+
+    @property
+    def close(self):
+        return f"</{self.HTML_TAG}>"
+
+    def eval(self):
+        return f"{self.open}{self.value.eval()}{self.close}"
+
+
+class BoldText(FormattedText):
+    HTML_TAG = "b"
+
+
+class ItalicText(FormattedText):
+    HTML_TAG = "i"
+
+
+class UnderlineText(FormattedText):
+    HTML_TAG = "u"
+
+
+class StrikethroughText(FormattedText):
+    HTML_TAG = "s"
