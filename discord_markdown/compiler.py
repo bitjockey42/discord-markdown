db5bdb05a4503b2ca1d1c7dcf7505f5d6c506f6e
BitJockey <bitjockey@jackpoint.network>
2020-03-22 21:07:04 -0500

    Basic compiler

---
+++ discord_markdown/compiler.py
@@ -0,0 +1,33 @@
+from .lexer import tokenize
+from .parser import Parser
+
+
+class Compiler:
+    def __init__(self, text):
+        self._text = text
+        self._tokens = tokenize(self._text)
+        self._parser = Parser(self._tokens)
+        self._code = ""
+
+    @property
+    def text(self):
+        return self._text
+
+    @text.setter
+    def text(self, text):
+        self._text = text
+        self._tokens = tokenize(self._text)
+
+    @property
+    def tokens(self):
+        return self._tokens
+
+    @property
+    def code(self):
+        return self._code
+
+    def compile(self):
+        self._parser.parse()
+        for node in self._parser.tree:
+            self._code = self._code + node.eval()
+        return self._code 
