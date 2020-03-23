11b41fffa8fb3577d27d1bf0752c59363abb54a4
BitJockey <bitjockey@jackpoint.network>
2020-03-22 21:22:19 -0500

    test for code and inline quote

--- tests/test_compiler.py
+++ tests/test_compiler.py
@@ -14,3 +14,23 @@ def test_compile_formatted():
         compiler.compile()
         == "Here I <i>am</i> in the <b>light</b> of <b><i>day</i></b>\nLet the storm rage on"
     )
+
+
+def test_compile_code_block():
+    text = """```sh
+    echo test
+    ```"""
+    compiler = Compiler(text)
+    assert compiler.compile() == "<pre><code>\n    echo test\n    </code></pre>"
+
+
+def test_compile_block_quote():
+    text = ">>> This is a quote.\nThis should be part of it."
+    compiler = Compiler(text)
+    assert compiler.compile() == "<blockquote> This is a quote.\nThis should be part of it.</blockquote>"
+
+
+def test_compile_inline_quote():
+    text = "> This is a quote.\nAnd this should not be part of it"
+    compiler = Compiler(text)
+    assert compiler.compile() == "<q> This is a quote.</q>And this should not be part of it"
