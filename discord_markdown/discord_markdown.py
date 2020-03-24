53b9a2abd162fd293c0a4f782997daff83c17bc7
BitJockey <bitjockey@jackpoint.network>
2020-03-23 21:37:20 -0500

    Add function to get the parse tree

--- discord_markdown/discord_markdown.py
+++ discord_markdown/discord_markdown.py
@@ -1,5 +1,14 @@
 from .compiler import Compiler
+from .parser import Parser
+from .lexer import tokenize
 
 
 def convert_to_html(text):
     return Compiler(text).compile()
+
+
+def parse_text(text):
+    tokens = tokenize(text)
+    parser = Parser(tokens)
+    parser.parse()
+    return parser.tree
