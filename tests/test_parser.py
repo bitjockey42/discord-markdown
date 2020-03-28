ea48ab5cae4239a8bd11d5ef41e4868f0970975a
BitJockey <bitjockey@jackpoint.network>
2020-03-27 23:20:15 -0500

    Test for complex markup

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -301,3 +301,48 @@ def test_code_block_2():
             ),
         ],
     )
+
+
+def test_complex_markup():
+    text = "[_Tiger looks at Kalahan contemplatively_]\nTiger: [_quietly_] ```asciidoc\n= Had only Bull not gotten to you first... =```\nTiger: ```asciidoc\n= You may do so. I simply wish her safe in her den. But I cannot and will not force you to do anything. And my power in the physical plane is greatly limited without one to call me mentor. =```"
+    tokens = tokenize(text)
+    parser = Parser(tokens)
+    parser.parse()
+    # assert_tree(parser.tree, [], markdown=True)
+    assert_tree(
+        parser.tree,
+        [
+            ast.Paragraph(
+                [
+                    ast.Text("["),
+                    ast.ItalicText(
+                        ast.Text("Tiger looks at Kalahan contemplatively"), md_tag="_"
+                    ),
+                    ast.Text("]"),
+                ]
+            ),
+            ast.Paragraph(
+                [
+                    ast.Text("Tiger: ["),
+                    ast.ItalicText(ast.Text("quietly"), md_tag="_"),
+                    ast.Text("] "),
+                    ast.CodeBlock(
+                        ast.Text("= Had only Bull not gotten to you first... ="),
+                        md_tag="```asciidoc",
+                    ),
+                ]
+            ),
+            ast.Paragraph(
+                [
+                    ast.Text("Tiger: "),
+                    ast.CodeBlock(
+                        ast.Text(
+                            "= You may do so. I simply wish her safe in her den. But I cannot and will not force you to do anything. And my power in the physical plane is greatly limited without one to call me mentor. ="
+                        ),
+                        md_tag="```asciidoc",
+                    ),
+                ]
+            ),
+        ],
+        markdown=True,
+    )
