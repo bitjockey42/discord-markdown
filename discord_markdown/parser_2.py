1c206550a12da7042f28367e49d3539ad857ccc6
BitJockey <bitjockey@jackpoint.network>
2020-03-30 00:53:15 -0500

    Create paragraph

--- discord_markdown/parser_2.py
+++ discord_markdown/parser_2.py
@@ -10,6 +10,11 @@ class BetterParser:
         self.string = string
         self.tokens = tokenize(string) 
         self._stack = []
+        self._tree = []
+
+    @property
+    def tree(self):
+        return self._tree
 
     def parse(self):
         print("STRING", self.string)
@@ -18,6 +23,7 @@ class BetterParser:
         token_iter = iter(self.tokens)
         current_token = next(token_iter, STOP)
         format_token = None
+        paragraph = None
 
         for token in self.tokens:
             print(token)
@@ -28,6 +34,10 @@ class BetterParser:
                 self._stack.append(current_token)
                 print(f"[{current_token.type}]")
                 current_token = next(token_iter, STOP)
+            else:
+                if paragraph is None:
+                    paragraph = ast.Paragraph()
+                paragraph.elements.append(ast.Text(current_token.value))
 
             while self._stack and current_token != STOP:
                 if current_token.type in FORMAT_TOKEN_TYPES:
@@ -44,6 +54,8 @@ class BetterParser:
 
             if current_token.type in TERMINAL_TOKEN_TYPES:
                 print("---------------------------------")
+                self._tree.append(paragraph)
+                paragraph = None
 
             if current_token != STOP:
                 print(current_token.value)
